d95c23dc8791b88a649eecde1a4be9a47a075af0
statecs <statec@icloud.com>
2015-02-12 19:39:31 +0100

    Fix and update
    fixed white page
    The content is now dynamically added (mainView.js)
    dishToSelectView.js tried but failed to update the ingredient

--- js/View/dishToSelectView.js
+++ js/View/dishToSelectView.js
@@ -2,6 +2,22 @@ var DishToSelectView = function (container, model) {
 
 	this.container = container;
 	
+	var dishesContainer = this.dishesContainer = container.find(".ing");
 
 
+	var loadDishes = function() {
+
+       var allIngredients = model.getAllIngredients();
+
+        $.each(allIngredients, function(key, ingredient) {
+            var stringToAdd = "<span class='floatleft'>" + 
+            ingredient.unit + "</span><span class='marginleft'>" + 
+            ingredient.name + "</span><span class='floatright'>" + 
+
+            dishesContainer.append("<label id='" + ingredient.id + ">" + stringToAdd + "</div>");
+        });
+
+
+	}
+	loadDishes();
 }
