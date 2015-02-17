9edc97df400227d1299578ec85f2f0574fde5a57
Joakim Larsson <joakimlarsson92@gmail.com>
2015-02-17 18:16:08 +0100

    Dynamic view for PrintMenuView
    Added dynamic view for printMenuView
    
    Issues:
    - Menu is unsorted

--- js/View/printMenuView.js
+++ js/View/printMenuView.js
@@ -1,4 +1,28 @@
 var PrintMenuView = function(container, model) {
 
 	this.container = container;
+
+	var dishesContainer = container.find(".page-6-content");
+	var numberOfGuests = $(".page-bar").find("h3");
+
+	model.addObserver(this);
+
+	var loadPrintMenu = function(){
+		dishesContainer.html("");
+		numberOfGuests.html("");
+
+		numberOfGuests.append("<h3>My Dinner: " + model.getNumberOfGuests() + " people</h3>");
+		$.each(model.getFullMenu(), function(key, dish) {
+        	var stringToAdd = "<div class='col-md-2'><img src='images/" + dish.image + "'></img></div>" +
+					"<div class='col-md-3'><h3>" + dish.name + "</h3><p>INGREDIENTS</p></div>" + 
+        			"<div class='col-md-5'><h4>Preparations</h4><p class='description'>" + dish.description + "</p></div>";
+
+        	dishesContainer.append("<div class='row'>" + stringToAdd + "</div>");
+
+        });
+	};
+
+	this.update = function(){
+		loadPrintMenu();
+	};
 }
