c9c6ceb503c946b18db0cbf115994d88072537fa
statecs <statec@icloud.com>
2015-03-02 14:37:45 +0100

    Views updated
    Layout done for all views.

--- lab5/app/js/app.js
+++ lab5/app/js/app.js
@@ -48,6 +48,15 @@ dinnerPlannerApp.config(['$routeProvider',
         templateUrl: 'partials/dish.html',
         controller: 'DishCtrl'
       }).
+        when('/overview', {
+        templateUrl: 'partials/overview.html',
+        controller: 'OverviewCtrl'
+      }).
+      when('/preparation', {
+        templateUrl: 'partials/preparation.html',
+        controller: 'PrepCtrl'
+      }).
+
       // TODO in Lab 5: add more conditions for the last two screens (overview and preparation)
       otherwise({
         redirectTo: '/home'
--- lab5/app/js/controllers/searchCtrl.js
+++ lab5/app/js/controllers/searchCtrl.js
@@ -4,7 +4,7 @@ dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
 
   // TODO in Lab 5: you will need to implement a method that searchers for dishes
   // including the case while the search is still running.
-
+  
   $scope.search = function(query) {
    $scope.status = "Searching...";
    Dinner.DishSearch.get({title_kw:query},function(data){
@@ -14,5 +14,6 @@ dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
      $scope.status = "There was an error";
    });
  }
+
 
 });
