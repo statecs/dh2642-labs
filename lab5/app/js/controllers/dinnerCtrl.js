88819d1d64fa25e85515dd25f2ccac45ba3aa495
statecs <statec@icloud.com>
2015-03-04 15:36:01 +0100

    Cookies :)

--- lab5/app/js/controllers/dinnerCtrl.js
+++ lab5/app/js/controllers/dinnerCtrl.js
@@ -1,6 +1,18 @@
 // Dinner controller that we use whenever we have view that needs to 
 // display or modify the dinner menu
-dinnerPlannerApp.controller('DinnerCtrl', function ($scope,Dinner) {
+dinnerPlannerApp.controller('DinnerCtrl', function ($scope,Dinner,$cookies,$cookieStore) {
+
+
+ $scope.bump = function () {
+        var lastVal = $cookieStore.get('lastValue');
+        if (!lastVal) {
+            $scope.lastVal = 1;
+        } else {
+            $scope.lastVal = lastVal + 1;
+        }
+        $cookieStore.put('lastValue', $scope.lastVal);
+    }
+    
 
   $scope.fullMenu = Dinner.getFullMenu();
 
--- lab5/app/js/controllers/searchCtrl.js
+++ lab5/app/js/controllers/searchCtrl.js
@@ -6,12 +6,11 @@ dinnerPlannerApp.controller('SearchCtrl',function($scope,$http,Dinner){
   // TODO in Lab 5: you will need to implement a method that searchers for dishes
   // including the case while the search is still running.
   
-  
 load_recipes();
 function load_recipes(){
 
-$http.get("http://api.bigoven.com/recipes?api_key=dvxjQjPAhbmCkz236n860N99N6441Zb2&pg=1&rpp=25").success(function(data){
-$scope.load_recipes=data.Results;
+  $http.get("http://api.bigoven.com/recipes?api_key=dvxjQjPAhbmCkz236n860N99N6441Zb2&pg=1&rpp=25").success(function(data){
+  $scope.load_recipes=data.Results;
 
 
  $scope.search = function(query) {
