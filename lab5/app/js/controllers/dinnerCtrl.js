86256b742732135b66e22611ed9f55d4c05784b2
Joakim Larsson <joakimlarsson92@gmail.com>
2015-03-05 22:03:34 +0100

    Nothing working yet, beginning of dishId

--- lab5/app/js/controllers/dinnerCtrl.js
+++ lab5/app/js/controllers/dinnerCtrl.js
@@ -8,28 +8,32 @@ dinnerPlannerApp.controller('DinnerCtrl', function ($scope,Dinner,$cookies,$cook
         $cookieStore.put('numberOfGuests', number);
         console.log($cookieStore.get('numberOfGuests'));
   }
-  
-  //Get dinner menu from Dinner Service  
+  var dinnerMenuIdCookie = $cookieStore.get('dinnerMenuId') || [];
+  console.log(dinnerMenuIdCookie);
+  $.each(dinnerMenuIdCookie, function(key, value){
+      console.log("Add dish from cookie to menu: " + value);
+      Dinner.addDishToMenu(Dinner.Dish.get({id:value}));
+  });
+
+  //Get dinner menu from Dinner Service, (!need to use cookies!)  
   $scope.fullMenu = Dinner.getFullMenu();
 
-  //Getting number of guests, loads from cookie if defined
-  if($cookieStore.get('numberOfGuests') === undefined){
-    $scope.numberOfGuests = Dinner.getNumberOfGuests();
-  }
-  else{
-    $scope.numberOfGuests = $cookieStore.get('numberOfGuests');
-  }
+  //Getting number of guests, loads from cookie if defined or use Dinner service
+  $scope.numberOfGuests = $cookieStore.get('numberOfGuests') || Dinner.getNumberOfGuests();
   
   //Remove dish from dinner menu
   $scope.removeDishFromMenu = function(event){
     Dinner.removeDishFromMenu(event.target.id);
   }
 
-
+  //Get total price of dinner menu
   $scope.getTotalMenuPrice = function() {
     return Dinner.getTotalMenuPrice();
   }
   
+  /*Get price of single dish 
+    IN: dish object
+    OUT: total price of dish */
   $scope.getPriceOfDish = function(dish) {
     var totalPriceOfDish = 0;
     $.each(dish.Ingredients, function(key, ingredient) {
--- lab5/app/js/controllers/dishCtrl.js
+++ lab5/app/js/controllers/dishCtrl.js
@@ -1,45 +1,29 @@
 // Dinner controller that we use whenever we want to display detailed
 // information for one dish
 dinnerPlannerApp.controller('DishCtrl', function ($scope,$routeParams,Dinner,$cookies,$cookieStore) {
-  
-  // TODO in Lab 5: you need to get the dish according to the routing parameter
-  // $routingParams.paramName
-  // Check the app.js to figure out what is the paramName in this case
 
 	//Need to improve error handling like in search control
 	$scope.selectedDish = Dinner.Dish.get({id:$routeParams.dishId});
 	
  //Litening to changes of number of guests in the Dinner Service
- $scope.$watch(function() { return Dinner.getNumberOfGuests(); }, function(lastVal) { 
-    /* Do the stuff */
+ $scope.$watch(function() { return Dinner.getNumberOfGuests(); }, function(lastVal) {
      $scope.numberOfGuests = lastVal;
  }, true);
 
   $scope.getPriceOfSelectedDish = function(){
-  var totalPriceOfDish = 0;
-  console.log($scope.selectedDish.Ingredients);
-  $.each($scope.selectedDish.Ingredients, function(key, ingredient) {
-    totalPriceOfDish += ingredient.Quantity*$scope.numberOfGuests;
-  });   
-  return totalPriceOfDish;
+    var totalPriceOfDish = 0;
+    $.each($scope.selectedDish.Ingredients, function(key, ingredient) {
+      totalPriceOfDish += ingredient.Quantity*$scope.numberOfGuests;
+    });   
+    return totalPriceOfDish;
   }
 
   $scope.addDishToMenu = function(e){
-
-  	console.log($scope.selectedDish);
-
-  	Dinner.addDishToMenu($scope.selectedDish);
-
-    var addDish = $cookieStore.get('dishId');
-
-        if (!addDish) {
-            $scope.addDish = $scope.selectedDish.RecipeID;
-        } else {
-            $scope.addDish = $scope.selectedDish.RecipeID;
-        }
-        $cookieStore.put('dishId', $scope.addDish);
-
-
+  	//Dinner.addDishToMenu($scope.selectedDish);
+    var dinnerMenuToUpdate = $cookieStore.get('dinnerMenuId') || [];
+    dinnerMenuToUpdate.push($scope.selectedDish.RecipeID);
+    console.log(dinnerMenuToUpdate);
+    $cookieStore.put('dinnerMenuId', dinnerMenuToUpdate);
   }
 
 
